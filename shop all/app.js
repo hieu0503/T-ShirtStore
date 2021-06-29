a0b8088686922b78ff8bd0ab0c075152ba1be363
hieudz99 <minhhieu050399@gmail.com>
2021-06-30 00:17:05 +0700

    fix loi grid

--- shop all/app.js
+++ shop all/app.js
@@ -5,7 +5,8 @@ fetch('http://localhost:3000/product')
     .then(data => {
         for (const key of data) {
             product.innerHTML += `
-        <div class="img">
+            <div class="product-card">
+            <div class="img">
             <img class="img-product" src="${key.img}" alt="">
             <img class="img-product-hover" src="${key.imgHover}" alt="">
             <a href="">Quick View</a>
@@ -13,6 +14,7 @@ fetch('http://localhost:3000/product')
         <p class="product-title">${key.title}</p>
         <p class="price">${key.price}</p>
         <p class="sell">Best Seller</p>
+            </div>
         `
         }
 
