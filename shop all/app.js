a73de225b5f5778e413970251f8d0da3ffa37159
hieudz99 <minhhieu050399@gmail.com>
2021-07-01 18:17:34 +0700

    update sale,men,women

--- shop all/app.js
+++ shop all/app.js
@@ -1,9 +1,9 @@
 const product = document.getElementById("product");
 const sell = document.getElementsByClassName("sell");
 
-fetch('http://localhost:3000/product')
-    .then(response => response.json())
-    .then(data => {
+fetch("http://localhost:3000/product")
+    .then((response) => response.json())
+    .then((data) => {
         for (const key of data) {
             product.innerHTML += `
             <div class="product-card">
@@ -16,16 +16,15 @@ fetch('http://localhost:3000/product')
                 <p class="price">${key.price}</p>
                 <p class="sell">Best Seller</p>
             </div>
-        `
+        `;
         }
     });
 
-    fetch('http://localhost:3000/product')
-    .then(response => response.json())
-    .then(data => {
-        for (const ty in data) {
-            let type = data.filter((ty) => ty.data === "men");
-            console.log(type);
+fetch("http://localhost:3000/product")
+    .then((response) => response.json())
+    .then((data) => {
+        function filterItems(items, searchVal) {
+            return items.filter((item) => Object.values(item).includes(searchVal));
         }
-    }
-    );
+        console.log("Filtered by type: ", filterItems(data, "men"));
+    });
